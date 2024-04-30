1a8f3a1ff629a81ba1664546c95aecc7fd9fc8de
SHIYUYAN7 <1871014238@qq.com>
2024-04-30 17:06:42 -0400

    structure

---
+++ index.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('header').innerHTML = '<object type="text/html" data="header/header.html" style="width:100%; height:100%;"></object>';
+    document.getElementById('footer').innerHTML = '<object type="text/html" data="footer/footer.html" style="width:100%; height:100%;"></object>';
+});
