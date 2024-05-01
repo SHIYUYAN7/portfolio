db26047d74425e611ea4d567089437ec7f5643e4
SHIYUYAN7 <1871014238@qq.com>
2024-05-01 15:27:19 -0400

    finish header

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('work').innerHTML = '<object type="text/html" data="work/work.html" style="width:100%; height:100%;"></object>';
-    document.getElementById('experience').innerHTML = '<object type="text/html" data="experience/experience.html" style="width:100%; height:100%;"></object>';
+    const workButton = document.getElementById('workButton');
+    const experienceButton = document.getElementById('experienceButton');
+    const workDiv = document.getElementById('work');
+    const experienceDiv = document.getElementById('experience');
+
+    // work
+    workButton.addEventListener('click', function() {
+        if (workDiv.innerHTML === "") {  
+            workDiv.innerHTML = '<object type="text/html" data="work/work.html" style="width:100%; height:100%;"></object>';
+        }
+        workDiv.style.display = 'block';
+        experienceDiv.style.display = 'none';
+    });
+
+    // experience
+    experienceButton.addEventListener('click', function() {
+        if (experienceDiv.innerHTML === "") { 
+            experienceDiv.innerHTML = '<object type="text/html" data="experience/experience.html" style="width:100%; height:100%;"></object>';
+        }
+        experienceDiv.style.display = 'block';
+        workDiv.style.display = 'none';
+    });
 });
