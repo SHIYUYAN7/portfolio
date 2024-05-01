6abc40b1a1ae8a0ec272d51e6a80970be24958f3
SHIYUYAN7 <1871014238@qq.com>
2024-05-01 14:35:14 -0400

    update framework

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('header').innerHTML = '<object type="text/html" data="header/header.html" style="width:100%; height:100%;"></object>';
-    document.getElementById('footer').innerHTML = '<object type="text/html" data="footer/footer.html" style="width:100%; height:100%;"></object>';
+    document.getElementById('work').innerHTML = '<object type="text/html" data="work/work.html" style="width:100%; height:100%;"></object>';
+    document.getElementById('experience').innerHTML = '<object type="text/html" data="experience/experience.html" style="width:100%; height:100%;"></object>';
 });
