085202adc7df4b11633b4d3069286b2a34c4b3e1
SHIYUYAN7 <1871014238@qq.com>
2024-05-06 18:37:58 -0400

    update

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    const homeButton = document.getElementById('homeButton');
     const workButton = document.getElementById('workButton');
     const experienceButton = document.getElementById('experienceButton');
+    const homeDiv = document.getElementById('home');
     const workDiv = document.getElementById('work');
     const experienceDiv = document.getElementById('experience');
 
-    // work
+    // Set default display to 'home'
+    homeDiv.style.display = 'flex';
+    workDiv.style.display = 'none';
+    experienceDiv.style.display = 'none';
+
+    // Home
+    homeButton.addEventListener('click', function() {
+        homeDiv.style.display = 'flex';
+        workDiv.style.display = 'none';
+        experienceDiv.style.display = 'none';
+    });
+
+    // Work
     workButton.addEventListener('click', function() {
         if (workDiv.innerHTML === "") {  
             workDiv.innerHTML = '<object type="text/html" data="work/work.html" style="width:100%; height:100%;"></object>';
         }
+        homeDiv.style.display = 'none';
         workDiv.style.display = 'block';
         experienceDiv.style.display = 'none';
     });
 
-    // experience
+    // Experience
     experienceButton.addEventListener('click', function() {
         if (experienceDiv.innerHTML === "") { 
             experienceDiv.innerHTML = '<object type="text/html" data="experience/experience.html" style="width:100%; height:100%;"></object>';
         }
+        homeDiv.style.display = 'none';
         experienceDiv.style.display = 'block';
         workDiv.style.display = 'none';
     });
