999243bc97fc7f078a43331be03cd023f1b8ec68
shiyu <1871014238@qq.com>
2024-05-07 15:29:23 -0400

    update code structure and format

--- index.js
+++ index.js
@@ -20,21 +20,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     // Work
     workButton.addEventListener('click', function() {
-        if (workDiv.innerHTML === "") {  
-            workDiv.innerHTML = '<object type="text/html" data="work/work.html" style="width:100%; height:100%;"></object>';
-        }
         homeDiv.style.display = 'none';
-        workDiv.style.display = 'block';
+        workDiv.style.display = 'flex';
         experienceDiv.style.display = 'none';
     });
 
     // Experience
     experienceButton.addEventListener('click', function() {
-        if (experienceDiv.innerHTML === "") { 
-            experienceDiv.innerHTML = '<object type="text/html" data="experience/experience.html" style="width:100%; height:100%;"></object>';
-        }
         homeDiv.style.display = 'none';
-        experienceDiv.style.display = 'block';
+        experienceDiv.style.display = 'flex';
         workDiv.style.display = 'none';
     });
 });
