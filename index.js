7248f9e49a4163e66f042c2326406753c1a91178
shiyu <1871014238@qq.com>
2024-05-10 13:04:04 -0400

    part finish work page

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    // header button
     const introButton = document.getElementById('introButton');
     const workButton = document.getElementById('workButton');
-    const experienceButton = document.getElementById('experienceButton');
+    const photographyButton = document.getElementById('photographyButton');
+
+    // maincontent
     const introDiv = document.getElementById('intro');
     const workDiv = document.getElementById('work');
-    const experienceDiv = document.getElementById('experience');
+    const photographyDiv = document.getElementById('photography');
 
+    // intro content button cick to work
     const exploreButton = document.getElementById('exploreButton');
+
+    // work content button cick to photography
+    const photographyCard = document.getElementById('photography-card');
 
     // Set default display to 'home'
     introDiv.style.display = 'none';
     workDiv.style.display = 'flex';
-    experienceDiv.style.display = 'none';
+    photographyDiv.style.display = 'none';
+
+    // photography-card Button
+    photographyCard.addEventListener('click', function() {
+        introDiv.style.display = 'none';
+        workDiv.style.display = 'none';
+        photographyDiv.style.display = 'flex';
+    });
 
     // Explore Button
     exploreButton.addEventListener('click', function() {
         introDiv.style.display = 'none';
         workDiv.style.display = 'flex';
-        experienceDiv.style.display = 'none';
+        photographyDiv.style.display = 'none';
     });
 
     // Home
     introButton.addEventListener('click', function() {
         introDiv.style.display = 'flex';
         workDiv.style.display = 'none';
-        experienceDiv.style.display = 'none';
+        photographyDiv.style.display = 'none';
     });
 
     // Work
     workButton.addEventListener('click', function() {
         introDiv.style.display = 'none';
         workDiv.style.display = 'flex';
-        experienceDiv.style.display = 'none';
+        photographyDiv.style.display = 'none';
     });
 
-    // Experience
-    experienceButton.addEventListener('click', function() {
+    // photography
+    photographyButton.addEventListener('click', function() {
         introDiv.style.display = 'none';
-        experienceDiv.style.display = 'flex';
+        photographyDiv.style.display = 'flex';
         workDiv.style.display = 'none';
     });
 });
