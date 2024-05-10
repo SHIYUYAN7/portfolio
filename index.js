a4fcc9492b3fc8e1776ebec5910efa7e61b71519
shiyu <1871014238@qq.com>
2024-05-10 15:16:26 -0400

    update about page option

--- index.js
+++ index.js
@@ -3,42 +3,70 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const introButton = document.getElementById('introButton');
     const workButton = document.getElementById('workButton');
     const photographyButton = document.getElementById('photographyButton');
+    const aboutButton = document.getElementById('aboutButton');
 
-    // maincontent
+    // main content
     const introDiv = document.getElementById('intro');
     const workDiv = document.getElementById('work');
     const photographyDiv = document.getElementById('photography');
+    const aboutDiv = document.getElementById('about');
+    const responsiveDiv = document.getElementById('responsive-case');
 
     // intro content button cick to work
     const exploreButton = document.getElementById('exploreButton');
 
-    // work content button cick to photography
+    // work content button cick to details
     const photographyCard = document.getElementById('photography-card');
+    const responsiveCard = document.getElementById('responsive-card');
 
     // Set default display to 'home'
     introDiv.style.display = 'none';
     workDiv.style.display = 'flex';
     photographyDiv.style.display = 'none';
+    aboutDiv.style.display = 'none';
+    responsiveDiv.style.display = 'none';
 
-    // photography-card Button
-    photographyCard.addEventListener('click', function() {
-        introDiv.style.display = 'none';
-        workDiv.style.display = 'none';
-        photographyDiv.style.display = 'flex';
-    });
 
     // Explore Button
     exploreButton.addEventListener('click', function() {
         introDiv.style.display = 'none';
         workDiv.style.display = 'flex';
         photographyDiv.style.display = 'none';
+        aboutDiv.style.display = 'none';
+        responsiveDiv.style.display = 'none';
     });
+
+    // ------------------------------------------work page card buttons------------------------------------------
+    
+    // responsive-card Button
+    responsiveCard.addEventListener('click', function() {
+        introDiv.style.display = 'none';
+        workDiv.style.display = 'none';
+        photographyDiv.style.display = 'none';
+        aboutDiv.style.display = 'none';
+        responsiveDiv.style.display = 'flex';
+    });
+
+    // photography-card Button
+    photographyCard.addEventListener('click', function() {
+        introDiv.style.display = 'none';
+        workDiv.style.display = 'none';
+        photographyDiv.style.display = 'flex';
+        aboutDiv.style.display = 'none';
+        responsiveDiv.style.display = 'none';
+    });
+    
+
+
+    // ------------------------------------------header buttons------------------------------------------
 
     // Home
     introButton.addEventListener('click', function() {
         introDiv.style.display = 'flex';
         workDiv.style.display = 'none';
         photographyDiv.style.display = 'none';
+        aboutDiv.style.display = 'none';
+        responsiveDiv.style.display = 'none';
     });
 
     // Work
@@ -46,12 +74,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         introDiv.style.display = 'none';
         workDiv.style.display = 'flex';
         photographyDiv.style.display = 'none';
+        aboutDiv.style.display = 'none';
+        responsiveDiv.style.display = 'none';
     });
 
-    // photography
+    // photography area
     photographyButton.addEventListener('click', function() {
         introDiv.style.display = 'none';
         photographyDiv.style.display = 'flex';
         workDiv.style.display = 'none';
+        aboutDiv.style.display = 'none';
+        responsiveDiv.style.display = 'none';
     });
+
+    // about area
+    aboutButton.addEventListener('click', function() {
+        introDiv.style.display = 'none';
+        photographyDiv.style.display = 'none';
+        workDiv.style.display = 'none';
+        aboutDiv.style.display = 'flex';
+        responsiveDiv.style.display = 'none';
+    });
+
+
+
 });
